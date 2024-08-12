a5bb19f673fa49aae3111cd2f9e8e977eb29ad4c
AlexG <Alexanderg.savitr@gmail.com>
2024-08-12 09:09:22 -0500

    Add more properties to model

--- models/song.js
+++ models/song.js
@@ -2,6 +2,32 @@ import mongoose from "mongoose"
 
 const Schema = mongoose.Schema
 
+const songSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  artist: {
+    type: String,
+    required: true
+  },
+  wordAssociation: {
+    type: String,
+    required: true
+  },
+  albumCover: {
+    type: String,
+    default: "URL String of app Logo"
+  },
+  rank: {
+    type: Number,
+    min: 1,
+    max: 10,
+  }
+})
+
+const Song = mongoose.model('Song', songSchema)
+
 export {
-  
+  Song
 }
